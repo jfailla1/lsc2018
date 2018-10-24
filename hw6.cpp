ac65d2c18093588bb1d05702dde93dcb0c5ae18e
Blueberry <jfailla1@tulane.edu>
2018-10-24 15:37:59 -0500

    HW6

--- hw6.cpp
+++ hw6.cpp
@@ -5,39 +5,42 @@
 #include <iostream>
 #include <fstream>
 #include <Eigen/Core>
+using namespace std;
 
 int main(int argc, char **argv) {
-  //// Ask user to input initial values
-  ///////////////////////
-  //// YOUR CODE HERE
-  ///////////////////////
+  
+	double speed;
+	double angle;
+	cout << "Enter Speed: ";
+	cin >> speed;
+	cout << "Enter Angle ";
+	cin >> angle;
 
-  ///// Define velocity and Compute Initial Velocity
-  //////////////////////
-  //// YOUR CODE HERE
-  ///////////////////////
-
-  // Initialize
-  const double g = -9.8; // m/s^2
-  const double drag = 1;
-  const double weight = 1;
-  Eigen::Vector2d position;
-  position.setZero();
-  Eigen::Vector2d acceleration;
-  acceleration << 0.0, g;
-
-  // Simulation
-  std::ofstream fout;
-  fout.open("hw06.txt");
-  const double dt = 0.01;
-  double simulationTime = 0.0;
-  while(position[1] >= 0.0){
-    fout << simulationTime << " " << position[0] << " " << position[1] << " " << velocity[0] << " " << velocity[1] << std::endl;
-    /// Update
-    ////////////////////
-    /// YOUR CODE HERE
-    ////////////////////
-  }
-  fout.close();
-  return 0;
+	Eigen::Vector2d velocity;
+	velocity << (speed*cos(angle*M_PI/180)),(speed*sin(angle*M_PI/180));
+	  
+	// Initialize
+  	const double g = -9.8; // m/s^2
+ 	const double drag = 1;
+  	const double weight = 1;
+  	Eigen::Vector2d position;
+  	position.setZero();
+  	Eigen::Vector2d acceleration;
+  	acceleration << 0.0, g;
+	Eigen::Vector2d G;
+	
+  	// Simulation
+  	std::ofstream fout;
+  	fout.open("hw06.txt");
+  	const double dt = 0.01;
+  	double simulationTime = 0.0;
+  	while(position[1] >= 0.0){
+    		fout << simulationTime << " " << position[0] << " " << position[1] << " " << velocity[0] << " " << velocity[1] << std::endl;
+	 	position += velocity * dt;
+		velocity += acceleration * dt;
+		simulationTime += dt;
+		acceleration += G - drag*velocity.norm()*velocity; 	
+	}
+  	fout.close();
+  	return 0;
 }
